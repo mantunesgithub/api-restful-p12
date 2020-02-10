ccfa32e9b7ae64dd511697d578bd40945beea4bf
Marcio Antunes <mantunes001@gmail.com>
2020-02-10 14:41:26 -0300

    parametros de configuracao apllication-properties

---
+++ src/test/java/br/com/udemy/p09/P11ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.udemy.p09;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
